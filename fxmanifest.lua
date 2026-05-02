3d6e967192f3bef0f594bc7ef349da3e1865dc0b
BaskinB <54458253+BaskinB@users.noreply.github.com>
2026-05-02 11:16:12 -0500

    feat(server): added version checking.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,29 +1,30 @@
 fx_version("cerulean")
 games({ "gta5" })
 lua54("yes")
+version("1.0.2")
 client_script("@pulsar-core/exports/cl_error.lua")
 client_script("@pulsar-pwnzor/client/check.lua")
 
 data_file("SCALEFORM_DLC_FILE")("stream/int3232302352.gfx")
 
 client_scripts({
-	"config.lua",
-	"client/*.lua",
-	--'demo_games.lua',
+  "config.lua",
+  "client/*.lua",
+  --'demo_games.lua',
 })
 
 server_scripts({
-	"config.lua",
-	"server/*.lua",
+  "config.lua",
+  "server/*.lua",
 })
 
 ui_page("ui/dist/index.html")
 files({
-	"ui/dist/index.html",
-	"ui/dist/*.png",
-	"ui/dist/*.webp",
-	"ui/dist/*.js",
-	"ui/dist/*.mp3",
-	"ui/dist/*.ttf",
-	"stream/int3232302352.gfx",
+  "ui/dist/index.html",
+  "ui/dist/*.png",
+  "ui/dist/*.webp",
+  "ui/dist/*.js",
+  "ui/dist/*.mp3",
+  "ui/dist/*.ttf",
+  "stream/int3232302352.gfx",
 })
